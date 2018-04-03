7a82d1f37875124b841af0d8d7bbe64d16ad2b50
Filip <mattsson.filip@gmail.com>
2018-04-03 11:17:49 +0200

    Added class for rosbag recording in centralized_mpc.py

--- src/centralized_mpc.py
+++ src/centralized_mpc.py
@@ -364,6 +364,58 @@ class CentralizedMPC(object):
         return filename
 
 
+class RosbagRecorder(object):
+
+    def __init__(self, filename_prefix, filename_suffix, default_topic_name='topic'):
+        self.prefix = filename_prefix
+        self.suffix = filename_suffix
+
+        self.filename = ''
+        self.recording = False
+        self.default_topic_name = default_topic_name
+
+    def write(self, msg, topic_name=None):
+        """Write the message to the bag. """
+        if self.recording:
+
+            if topic_name is None:
+                topic_name = self.default_topic_name
+
+            try:
+                self.bag.write(topic_name, msg)
+            except ValueError as e:
+                print('Error when writing to bag: {}'.format(e))
+
+    def start(self):
+        """Starts the recording. Creates a new bag. """
+        if not self.recording:
+            self.recording = True
+            self.filename = self._get_unique_filename(self.prefix, self.suffix)
+            self.bag = rosbag.Bag(self.filename, 'w')
+
+    def stop(self):
+        """Stops the recording. Closes the bag. """
+        if self.recording:
+            self.recording = False
+            self.bag.close()
+
+    @staticmethod
+    def _get_unique_filename(prefix, suffix, padding=0):
+        """Sets a filename on the form filename_prefixZ.bag where Z is the first free number.
+        Pads with zeros, e.g. first free number 43 and padding=5 will give 00043. """
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+        i = 0
+        while os.path.exists(os.path.join(__location__, '{}{}{}'.format(
+                prefix, str(i).zfill(padding), suffix))):
+            i += 1
+
+        filename = os.path.join(__location__, '{}{}{}'.format(
+            prefix, str(i).zfill(padding), suffix))
+
+        return filename
+
+
 def main(args):
 
     if len(args) > 1:
